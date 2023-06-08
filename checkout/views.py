4f83aa72e99c4c3be009b5a91b2e6c58cf931761
Bella-aa <Nyashamarara28@gmail.com>
2023-06-08 20:00:53 +0000

    added checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,18 +9,30 @@ from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    print('IN CHECKOUT VIEW')
-    print('stripe_public_key: ', stripe_public_key)
-    print('stripe_secret_key: ', stripe_secret_key)
-
     if request.method == 'POST':
-        print('POST REQUEST SENT')
         bag = request.session.get('bag', {})
 
         form_data = {
@@ -64,15 +77,11 @@ def checkout(request):
                     return redirect(reverse('view_bag'))
 
             request.session['save_info'] = 'save-info' in request.POST
-            print('ORDER: ', order)
-            print('ORDER NUMBER: ', order.order_number)
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            print('ERROR WITH FORM')
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
     else:
-        print('POST REQUEST NOT SENT')
         bag = request.session.get('bag', {})
         if not bag:
             messages.error(request, "There's nothing in your bag at the moment")
