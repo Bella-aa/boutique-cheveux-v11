73f1c143641e61b359f3640d738f5293c627f7f2
Bella-aa <Nyashamarara28@gmail.com>
2023-06-08 10:19:37 +0000

    added stripe elements. js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NGfYmKbtoiITpjby6AHWt54HiticaNMQFl07oG5Vspdb7fauVShXJMJx20TCcAk0Rfq4zKGKWSkBngsP0VXYRS400Cwxr9ItB',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
