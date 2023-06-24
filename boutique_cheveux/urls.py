063f57a0ec24674ff2b36b65e05a82287651fecc
Bella-aa <Nyashamarara28@gmail.com>
2023-06-24 18:50:39 +0000

    removed contact app and vivs and urls

--- boutique_cheveux/urls.py
+++ boutique_cheveux/urls.py
@@ -29,6 +29,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('review/', include('review.urls')),
-    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'boutique_cheveux.views.handler404'
